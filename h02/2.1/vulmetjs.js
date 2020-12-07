d1bb9a71d281a1240672b076016a828fd7c1672e
blazer1400 <sbroersma@roc-dev.com>
2020-12-07 12:07:39 +0100

    h02

---
+++ h02/2.1/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").setAttribute("src", "img/aap1.png");
+document.getElementById("aap2").setAttribute("src", "img/aap2.png");
---
+++ h02/2.2/index.js
@@ -0,0 +1,7 @@
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "40px";
